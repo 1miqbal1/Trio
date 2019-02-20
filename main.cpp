1f6f48818b0485575368b4281d8000dfb34b502f
1miqbal1 <miqbal1@mail.csuchico.edu>
2019-02-20 11:35:07 -0800

    added config file for travis

--- main.cpp
+++ main.cpp
@@ -1,40 +1,47 @@
 #include <iostream>
+#include <vector>
 using std::cout;
 using std::cin;
 using std::endl;
-
-void sortDescending(int,int,int);
+using std::vector;
+void sortDescending(vector<int>nums);
 void swap(int&,int&);
 
 int main()
 {
   int numA, numB, numC;
+  vector<int> num;
   cout<<"Enter any three numbers: ";
   cin>>numA>>numB>>numC;
-
-  sortDescending(numA, numB, numC);
-  
+  num.push_back(numA);
+  num.push_back(numB);
+  num.push_back(numC);
+  sortDescending(num);
   cout<<"From greatest to least, they are: ";
-  cout<<numA<<","<<numB<<","<<numC<<endl;
+  cout<<num.at(0)<<","<<num.at(1)<<","<<num.at(2)<<endl;
   return 0;
 }
-
-void sortDescending(int first, int second, int third)
+/*
+ * sort vector in descending order
+ */
+void sortDescending(vector<int>nums)
 {
-  if( first < third )
+  if( nums.at(0) < nums.at(2) )
   {
-    swap(first,third);
+    swap(nums.at(0), nums.at(2));
   }
-  if( first < second )
+  if( nums.at(0) < nums.at(1) )
   {
-    swap(first,second);
+    swap(nums.at(0),nums.at(1));
   }
-  if( second < third )
+  if( nums.at(1) < nums.at(2) )
   {
-    swap(second,third);
+    swap(nums.at(1),nums.at(2));
   }
 }
-
+/*
+ * swap integers
+ */
 void swap(int &first, int &second)
 {
   int temp = first;
